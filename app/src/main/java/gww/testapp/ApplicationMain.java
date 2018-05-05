d59f365d4d1c71d30780f694420c77349018982d
Logan <julian@jollycorp.com>
2018-05-05 08:54:09 +0800

    rxjava2提交

--- app/src/main/java/gww/testapp/ApplicationMain.java
+++ app/src/main/java/gww/testapp/ApplicationMain.java
@@ -3,12 +3,11 @@ package gww.testapp;
 import android.app.Application;
 
 /**
- * desc: TODO <br/>
+ * desc: 自定义Application <br/>
  * time: 2018/1/31 下午1:43 <br/>
  * author: Logan <br/>
- * since V TODO <br/>
+ * since V 1。0 <br/>
  */
-
 public class ApplicationMain extends Application {
 
     public static ApplicationMain mInstance;
@@ -16,9 +15,7 @@ public class ApplicationMain extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         mInstance = this;
-
         registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacksImpl());
     }
 
---
+++ app/src/main/java/gww/testapp/ui/rxjava2/Activity_1_SimpleDemo.java
@@ -0,0 +1,11 @@
+package gww.testapp.ui.rxjava2;
+
+/**
+ * desc: RxJava2简单例子 <br/>
+ * time: 2018/5/5 上午8:41 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+public class Activity_1_SimpleDemo {
+    
+}
