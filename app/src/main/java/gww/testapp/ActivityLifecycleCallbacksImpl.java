cd29e8fdd518c414d6296a0e1e345d5a4950ed95
Logan <julian@jollycorp.com>
2018-05-05 10:06:44 +0800

    rxjava2 simple demo

--- app/src/main/java/gww/testapp/ActivityLifecycleCallbacksImpl.java
+++ app/src/main/java/gww/testapp/ActivityLifecycleCallbacksImpl.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
 
-import gww.testapp.utils.ToolLog;
-
 /**
  * desc: Activity生命周期回调监听 <br/>
  * time: 2018/3/26 下午4:56 <br/>
@@ -16,37 +14,37 @@ public class ActivityLifecycleCallbacksImpl implements Application.ActivityLifec
 
     @Override
     public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        ToolLog.v("CallbacksImpl -> onCreated():" + activity.getClass().getSimpleName());
+        //ToolLog.v("CallbacksImpl -> onCreated():" + activity.getClass().getSimpleName());
     }
 
     @Override
     public void onActivityStarted(Activity activity) {
-        ToolLog.v("CallbacksImpl -> onStarted():" + activity.getClass().getSimpleName());
+        //ToolLog.v("CallbacksImpl -> onStarted():" + activity.getClass().getSimpleName());
     }
 
     @Override
     public void onActivityResumed(Activity activity) {
-        ToolLog.v("CallbacksImpl -> onResumed():" + activity.getClass().getSimpleName());
+        //ToolLog.v("CallbacksImpl -> onResumed():" + activity.getClass().getSimpleName());
     }
 
     @Override
     public void onActivityPaused(Activity activity) {
-        ToolLog.v("CallbacksImpl -> onPaused():" + activity.getClass().getSimpleName());
+        //ToolLog.v("CallbacksImpl -> onPaused():" + activity.getClass().getSimpleName());
     }
 
     @Override
     public void onActivityStopped(Activity activity) {
-        ToolLog.v("CallbacksImpl -> onStopped():" + activity.getClass().getSimpleName());
+        //ToolLog.v("CallbacksImpl -> onStopped():" + activity.getClass().getSimpleName());
     }
 
     @Override
     public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-        ToolLog.v("CallbacksImpl -> onSaveInstanceState():" + activity.getClass().getSimpleName());
+        //ToolLog.v("CallbacksImpl -> onSaveInstanceState():" + activity.getClass().getSimpleName());
     }
 
     @Override
     public void onActivityDestroyed(Activity activity) {
-        ToolLog.v("CallbacksImpl -> onDestroyed():" + activity.getClass().getSimpleName());
+        //ToolLog.v("CallbacksImpl -> onDestroyed():" + activity.getClass().getSimpleName());
     }
 
 }
--- app/src/main/java/gww/testapp/ui/MainActivity.java
+++ app/src/main/java/gww/testapp/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import gww.testapp.R;
 import gww.testapp.ui.lambda.ActivityLambda;
+import gww.testapp.ui.rxjava2.Activity_1_SimpleDemo;
 
 public class MainActivity extends Activity {
 
@@ -24,6 +25,10 @@ public class MainActivity extends Activity {
 
         findViewById(R.id.btn_lambda).setOnClickListener(v -> {
             startActivity(new Intent(this, ActivityLambda.class));
+        });
+
+        findViewById(R.id.btn_rxjava2_simple_demo).setOnClickListener(v -> {
+            startActivity(new Intent(this, Activity_1_SimpleDemo.class));
         });
     }
 
