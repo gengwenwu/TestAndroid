69b7b55293cf5a42c006aaf3ce270dd3fb25633b
Logan <490211581@qq.com>
2018-05-23 15:48:52 +0800

    FromArray测试

---
+++ app/src/main/java/gww/testapp/ui/rxjava2/operators/RxFromArray.java
@@ -0,0 +1,27 @@
+package gww.testapp.ui.rxjava2.operators;
+
+import gww.testapp.utils.ToolLog;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+/**
+ * desc: 发射特定数组的Observable <br/>
+ * time: 2018/5/23 下午3:44 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+public class RxFromArray implements IOperator {
+
+    @Override
+    public void test() {
+        Integer[] items = {0, 1, 2, 3, 4, 5};
+        Observable.fromArray(items)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        ToolLog.v("FromArray -> integer:" + integer);
+                    }
+                });
+    }
+
+}
